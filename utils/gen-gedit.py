5e9bd252ab8b163c9a97ff838fc827e4a59c2135
Curtis Hovey <curtis.hovey@canonical.com>
2008-06-22 10:42:25 -0400

    auto<re>conf; configure; make; make install "Just Works".

--- utils/gen-gedit.py
+++ utils/gen-gedit.py
@@ -5,7 +5,7 @@ import os
 
 
 fakes_dir = 'gedit'
-fake_defs = ['gedit', 'geditcommands', 'geditutils']
+fake_defs = ('gedit', 'geditcommands', 'geditutils')
 
 fakegen = 'fakegen.py'
 fakegen_path = '../testing/%s' % fakegen
@@ -15,4 +15,4 @@ print "Creating fake implementation in %s." % fakes_dir
 for defs in fake_defs:
     os.spawnl(os.P_WAIT, fakegen_path, fakegen, '%s.defs' % defs)
     print "Created %s." % defs
-print "Rerun gen-gedit.py whenever gedit's defs or overrides are updated"
+print "Rerun gen-gedit.py whenever gedit's defs or overrides are updated."
