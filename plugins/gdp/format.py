a21e77bff266f124e0994a019d03c47f23265bee
sinzui <>
2009-08-25 13:51:59 -0400

    Added Formatter to work with plugin. The sort import and line ending methods work.

--- plugins/gdp/format.py
+++ plugins/gdp/format.py
@@ -3,136 +3,164 @@
 
 import os
 import re
-import sys
 from textwrap import wrap
 
+class Formatter:
+    """Format Gedit Document and selection text."""
 
-def selection_to_one_line():
-    """Convert the selection to one line."""
-    lines = []
-    for line in os.sys.stdin:
-        lines.append(line.strip())
-    long_line = ' ' + ' '.join(lines)
-    os.sys.stdout.write(long_line.strip())
+    def __init__(self, window):
+        self.window = window
+
+    def _get_bounded_text(self):
+        """Return tuple of the bounds and formattable text.
+
+        The bounds mark either the selection or the document.
+        """
+        document = self.window.get_active_document()
+        bounds = document.get_selection_bounds()
+        if not bounds:
+            bounds = (document.get_start_iter(), document.get_end_iter())
+        text = document.get_text(*bounds)
+        return bounds, text
+
+    def _put_bounded_text(self, bounds, text):
+        """Replace the current text between the bounds with the new text.
+
+        This change is undoable.
+        """
+        document = self.window.get_active_document()
+        document.begin_user_action()
+        document.delete(*bounds)
+        document.insert_at_cursor(text)
+        document.end_user_action()
+
+    def selection_to_one_line(self):
+        """Convert the selection to one line."""
+        lines = []
+        for line in os.sys.stdin:
+            lines.append(line.strip())
+        long_line = ' ' + ' '.join(lines)
+        os.sys.stdout.write(long_line.strip())
+
+    def wrap_selection_list(self):
+        """Wrap long lines and preserve indentation."""
+        paras = []
+        indent_re = re.compile('^( +[0-9*-]*\.*)')
+        for line in os.sys.stdin:
+            match = indent_re.match(line)
+            if match is not None:
+                symbol = match.group(1)
+                # When there is not symbol in the indent, remove a space
+                # because a space is automatically added for the symbol.
+                if not symbol.replace(' ', ''):
+                    symbol = symbol[0:-1]
+            else:
+                symbol = ''
+            padding_size = len(symbol)
+            padding = ' ' * padding_size
+            run = 72 - padding_size
+            new_lines = []
+            new_line = [symbol]
+            new_length = padding_size
+            # ignore the symbol
+            words = line.split()
+            for word in words[1:]:
+                # The space between the words is 1
+                if new_length + 1 + len(word) > run:
+                    new_lines.append(' '.join(new_line))
+                    new_line = [padding]
+                    new_length = padding_size
+                new_line.append(word)
+                new_length += 1 + len(word)
+            new_lines.append(' '.join(new_line))
+            paras.extend(new_lines)
+        os.sys.stdout.write('\n'.join(paras))
 
 
-def wrap_selection_list():
-    """Wrap long lines and preserve indentation."""
-    paras = []
-    indent_re = re.compile('^( +[0-9*-]*\.*)')
-    for line in os.sys.stdin:
-        match = indent_re.match(line)
-        if match is not None:
-            symbol = match.group(1)
-            # When there is not symbol in the indent, remove a space
-            # because a space is automatically added for the symbol.
-            if not symbol.replace(' ', ''):
-                symbol = symbol[0:-1]
+    def wrap_selection_para(self, long_line=None, bounds=None):
+        """Wrap long lines."""
+        paras = []
+        indent_re = re.compile('^( +[0-9*-]\.*)')
+        if long_line:
+            source = long_line.split('\n')
         else:
-            symbol = ''
-        padding_size = len(symbol)
-        padding = ' ' * padding_size
-        run = 72 - padding_size
-        new_lines = []
-        new_line = [symbol]
-        new_length = padding_size
-        # ignore the symbol
-        words = line.split()
-        for word in words[1:]:
-            # The space between the words is 1
-            if new_length + 1 + len(word) > run:
-                new_lines.append(' '.join(new_line))
-                new_line = [padding]
-                new_length = padding_size
-            new_line.append(word)
-            new_length += 1 + len(word)
-        new_lines.append(' '.join(new_line))
-        paras.extend(new_lines)
-    os.sys.stdout.write('\n'.join(paras))
+            bounds, source = self._get_bounded_text()
+        for line in source:
+            match = indent_re.match(line)
+            if match is not None:
+                symbol = match.group(1)
+            else:
+                symbol = ''
+            padding_size = len(symbol)
+            padding = ' ' * padding_size
+            run = 72 - padding_size
+            new_lines = []
+            new_line = [symbol]
+            new_length = padding_size
+            # ignore the symbol
+            words = line.split()
+            if symbol != '':
+                words = words[1:]
+            for word in words:
+                # The space between the words is 1
+                if new_length + 1 + len(word) > run:
+                    new_lines.append(' '.join(new_line).strip())
+                    new_line = [padding]
+                    new_length = padding_size
+                new_line.append(word)
+                new_length += 1 + len(word)
+            new_lines.append(' '.join(new_line).strip())
+            paras.extend(new_lines)
+        self._put_bounded_text(bounds, '\n'.join(paras))
+
+    def newline_ending(self, data):
+        """Fix the selection's line endings."""
+        lines = []
+        bounds, text = self._get_bounded_text()
+        for line in text.splitlines():
+            lines.append(line.rstrip())
+        text = '\n'.join(lines)
+        self._put_bounded_text(bounds, text)
 
 
-def wrap_selection_para(long_line=None):
-    """Wrap long lines."""
-    paras = []
-    indent_re = re.compile('^( +[0-9*-]\.*)')
-    if long_line:
-        source = long_line.split('\n')
-    else:
-        source = os.sys.stdin
-    for line in source:
-        match = indent_re.match(line)
-        if match is not None:
-            symbol = match.group(1)
-        else:
-            symbol = ''
-        padding_size = len(symbol)
-        padding = ' ' * padding_size
-        run = 72 - padding_size
-        new_lines = []
-        new_line = [symbol]
-        new_length = padding_size
-        # ignore the symbol
-        words = line.split()
-        if symbol != '':
-            words = words[1:]
-        for word in words:
-            # The space between the words is 1
-            if new_length + 1 + len(word) > run:
-                new_lines.append(' '.join(new_line).strip())
-                new_line = [padding]
-                new_length = padding_size
-            new_line.append(word)
-            new_length += 1 + len(word)
-        new_lines.append(' '.join(new_line).strip())
-        paras.extend(new_lines)
-    os.sys.stdout.write('\n'.join(paras))
+    def _format_paragraph(self, paragraph, lines):
+        """Rewrap the paragraph and normalize leading blank lines."""
+        long_line = ' '.join(paragraph).strip()
+        paragraph_lines = wrap(long_line, 72)
+        # One blank line between blocks, except for headings, which
+        # should have two leading blanks
+        if len(lines) != 0 and long_line[0] == '=':
+            paragraph_lines.insert(0, '')
+        return paragraph_lines
 
 
-def newline_ending():
-    """Fix the selection's line endings."""
-    lines = []
-    for line in os.sys.stdin:
-        lines.append(line.rstrip())
-    text = '\n'.join(lines)
-    os.sys.stdout.write(text)
+    def quote_line(self):
+        """Quote the selected text passage."""
+        lines = []
+        for line in os.sys.stdin:
+            lines.append('> %s' % line)
+        os.sys.stdout.write(''.join(lines))
 
 
-def _format_paragraph(paragraph, lines):
-    """Rewrap the paragraph and normalize leading blank lines."""
-    long_line = ' '.join(paragraph).strip()
-    paragraph_lines = wrap(long_line, 72)
-    # One blank line between blocks, except for headings, which
-    # should have two leading blanks
-    if len(lines) != 0 and long_line[0] == '=':
-        paragraph_lines.insert(0, '')
-    return paragraph_lines
+    def rewrap_selection(self):
+        """Rewrap the selection."""
+        lines = []
+        for line in os.sys.stdin:
+            lines.append(line.strip())
+        long_line = ' ' + ' '.join(lines)
+        self.wrap_selection_para(long_line=long_line)
 
 
-def quote_line():
-    """Quote the selected text passage."""
-    lines = []
-    for line in os.sys.stdin:
-        lines.append('> %s' % line)
-    os.sys.stdout.write(''.join(lines))
-
-
-def rewrap_selection():
-    """Rewrap the selection."""
-    lines = []
-    for line in os.sys.stdin:
-        lines.append(line.strip())
-    long_line = ' ' + ' '.join(lines)
-    wrap_selection_para(long_line=long_line)
-
-
-def sort_imports():
-    """Sort python imports."""
-    lines = []
-    for line in os.sys.stdin:
-        lines.append(line.strip())
-    text = ' '.join(lines)
-    imports = text.split(', ')
-    imports = sorted(imports, key=str.lower)
-    imports = [imp.strip() for imp in imports if imp != '']
-    wrap_selection_para(', '.join(imports))
+    def sort_imports(self, data):
+        """Sort python imports."""
+        lines = []
+        bounds, text = self._get_bounded_text()
+        for line in text.splitlines():
+            lines.append(line.strip())
+        text = ' '.join(lines)
+        imports = text.split(', ')
+        imports = sorted(imports, key=str.lower)
+        imports = [imp.strip() for imp in imports if imp != '']
+        long_line = ', '.join(imports)
+        self.wrap_selection_para(long_line=long_line, bounds=bounds)
 
--- plugins/gdpformat.py
+++ plugins/gdpformat.py
@@ -13,10 +13,10 @@ from gettext import gettext as _
 import gedit
 import gtk
 
-from gdp import format
+from gdp.format import Formatter
 
 
-ui_xml = """
+menu_xml = """
 <ui>
   <menubar name="MenuBar">
     <menu name="EditMenu" action="Edit">
@@ -37,14 +37,22 @@ class FormatPlugin(gedit.Plugin):
     # This is a new-style class that call and old-style __init__().
     # pylint: disable-msg=W0233
 
-    _actions = [
-        # (name, stock_id, label, accelerator, tooltip, callback)
-        ('Format', None, _('_Format'), None, None, None),
-        ('FixLineEnding', None, _('Fix _line endings'), None,
-            _('Fix line endings'), format.newline_ending),
-        ('SortImports', None, _('_Sort imports'), None,
-            _('Sort imports'), format.sort_imports),
-        ]
+    @property
+    def _actions(self):
+        """Return a list of action tuples.
+
+        (name, stock_id, label, accelerator, tooltip, callback)
+        """
+        return  [
+            ('Format', None, _('_Format'), None, None, None),
+            ('FixLineEnding', None, _('Fix _line endings'), None,
+                _('Convert line endings to newlines and '
+                  'remove trailing whitespace'),
+                self.formatter.newline_ending),
+            ('SortImports', None, _('_Sort imports'), None,
+                _('Sort and wrap imports'),
+                self.formatter.sort_imports),
+            ]
 
     def __init__(self):
         """Initialize the plugin the whole Gedit application."""
@@ -54,15 +62,15 @@ class FormatPlugin(gedit.Plugin):
     def activate(self, window):
         """Activate the plugin in the current top-level window.
 
-        Add 'Format' to the edit menu.
+        Add 'Format' to the edit menu and create a Formatter.
         """
         self.window = window
-        #self.controller = FindController()
+        self.formatter = Formatter(window)
         self.action_group = gtk.ActionGroup("FormatActions")
         self.action_group.add_actions(self._actions)
         manager = self.window.get_ui_manager()
         manager.insert_action_group(self.action_group, 1)
-        self.ui_id = manager.add_ui_from_string(ui_xml)
+        self.ui_id = manager.add_ui_from_string(menu_xml)
 
     def deactivate(self, window):
         """Deactivate the plugin in the current top-level window."""
@@ -72,7 +80,7 @@ class FormatPlugin(gedit.Plugin):
         manager.ensure_update()
         self.ui_id = None
         self.action_group = None
-        self.controller = None
+        self.formatter = None
         self.window = None
 
     def update_ui(self, window):
