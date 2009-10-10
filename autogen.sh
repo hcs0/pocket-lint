a162d232f667f5c5a6702b5ff2a9f67127f30233
sinzui <>
2009-10-09 23:30:12 -0400

    autogen changes for upgrade to Karmic.

--- autogen.sh
+++ autogen.sh
@@ -10,12 +10,16 @@ PKG_NAME="gedit-developer-plugins"
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
-}
+    }
 
 which gnome-autogen.sh || {
     echo "You need to install gnome-common from your distro or gnome.org"
     exit 1
-}
+    }
 
-ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4" REQUIRED_AUTOMAKE_VERSION=1.8 REQUIRED_MACROS=python.m4 USE_GNOME2_MACROS=1 . gnome-autogen.sh
+ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4" \
+    REQUIRED_AUTOMAKE_VERSION=1.11 \
+    REQUIRED_MACROS=python.m4 \
+    USE_GNOME2_MACROS=1 \
+    . gnome-autogen.sh
 
