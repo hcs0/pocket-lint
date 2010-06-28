5c1928eea1c4552c9289906abff58214dd24423d
Curtis Hovey <sinzui.is@verizon.net>
2010-06-28 19:09:58 -0400

    Added distutils/

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+setup(
+    name="pocketlint",
+    version="0.1",
+    maintainer="Curtis C. Hovey",
+    maintainer_email="sinzui.is@verizon.net",
+    description="Pocket-lint a composite linter and style checker.",
+)
