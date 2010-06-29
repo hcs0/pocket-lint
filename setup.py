0703b443faffd945b961edee11b195387b495857
Curtis Hovey <sinzui.is@verizon.net>
2010-06-29 13:45:16 -0400

    Correct the version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
 setup(
     name="pocketlint",
     description="Pocket-lint a composite linter and style checker.",
-    version="0.1",
+    version="0.1.0",
     maintainer="Curtis C. Hovey",
     maintainer_email="sinzui.is@verizon.net",
     url="https://launchpad.net/pocket-lint",
