d155bb82140b13d2ebee09edaf043b0d262612b7
Curtis Hovey <sinzui.is@verizon.net>
2010-06-30 00:26:04 -0400

    Refactored jsreporter.

--- pocketlint/jsreporter.js
+++ pocketlint/jsreporter.js
@@ -1,5 +1,8 @@
+// Copyright (C) 2009-2010 - Curtis Hovey <sinzui.is at verizon.net>
+// This software is licensed under the MIT license (see the file COPYING).
+
 function report_implied_names() {
-    /* Report about implied global names. */
+    // Report about implied global names.
     var implied_names = [];
     for (var name in JSLINT.implied) {
         if (JSLINT.implied.hasOwnPropery(name)) {
@@ -7,29 +10,32 @@ function report_implied_names() {
             }
         }
     if (implied_names.length > 0) {
-        implied_names.sort();
+        implied_names.sort()
         print('0::0::Implied globals:' + implied_names.join(', '));
         }
     }
 
 
 function report_lint_errors() {
-    /* Report about lint errors. */
+    // Report about lint errors.
     for (var i = 0; i < JSLINT.errors.length; i++) {
         var error = JSLINT.errors[i];
         if (error === null) {
             print('0::0::JSLINT had a fatal error.');
             }
-        var line_no = error.line + 1;
-        var char_no = error.character + 1;
-        print(line_no + '::' + char_no + '::' + error.reason);
+        print(++error.line + '::' + ++error.character + '::' + error.reason);
         }
     }
 
 
-var source_script = arguments[0];
-var result = JSLINT(source_script);
-if (! result) {
-    report_lint_errors();
-    report_implied_names();
+function main(source_script) {
+    // Lint the source and report errors.
+    var result = JSLINT(source_script);
+    if (! result) {
+        report_lint_errors();
+        report_implied_names();
+        }
     }
+
+
+main(arguments[0])
