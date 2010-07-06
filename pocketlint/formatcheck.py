26e3de84d6c35d28eac1564a40eed51f4662996e
Curtis Hovey <sinzui.is@verizon.net>
2010-07-06 12:36:49 -0400

    match .doctest as mime-type.

--- pocketlint/formatcheck.py
+++ pocketlint/formatcheck.py
@@ -132,8 +132,10 @@ class Language:
 
     mimetypes.add_type('application/x-zope-configuation', '.zcml')
     mimetypes.add_type('application/x-zope-page-template', '.pt')
+    mimetypes.add_type('text/x-python-doctest', '.doctest')
     mime_type_language = {
         'text/x-python': PYTHON,
+        'text/x-python-doctest': DOCTEST,
         'text/css': CSS,
         'text/html': HTML,
         'text/plain': TEXT,
