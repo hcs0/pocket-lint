7809fe649415d4b5f6834f832387aa24f7b1c2d7
Curtis Hovey <sinzui.is@verizon.net>
2010-07-06 12:24:17 -0400

    Removed unused format module. Incremented the version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="pocketlint",
     description="Pocket-lint a composite linter and style checker.",
-    version="0.3.0",
+    version="0.4.0",
     maintainer="Curtis C. Hovey",
     maintainer_email="sinzui.is@verizon.net",
     url="https://launchpad.net/pocket-lint",
