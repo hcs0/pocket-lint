80ece87bce6a77783b2c9fe3da2c992b2cabd99e
Curtis Hovey <sinzui.is@verizon.net>
2010-08-03 09:20:46 -0400

    incremented version because of import change.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="pocketlint",
     description="Pocket-lint a composite linter and style checker.",
-    version="0.5.1",
+    version="0.5.2",
     maintainer="Curtis C. Hovey",
     maintainer_email="sinzui.is@verizon.net",
     url="https://launchpad.net/pocket-lint",
