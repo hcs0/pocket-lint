6570cccd533d7011b61629c98fa7f1f98fd4e68a
Curtis Hovey <sinzui.is@verizon.net>
2011-03-27 16:42:06 -0400

    Added a test verify entities are supported when there is no dtd.

--- pocketlint/tests/test_xml.py
+++ pocketlint/tests/test_xml.py
@@ -6,6 +6,22 @@ from pocketlint.tests import CheckerTestCase
 from pocketlint.tests.test_text import TestAnyTextMixin
 
 
+missing_dtd = """\
+<root>
+  <child>hello&nbsp;world</child>
+</root>
+"""
+
+
+class TestXML(CheckerTestCase):
+    """Verify XML integration."""
+
+    def test_missing_dtd(self):
+        checker = XMLChecker('bogus', missing_dtd, self.reporter)
+        checker.check()
+        self.assertEqual([], self.reporter.messages)
+
+
 class TestText(CheckerTestCase, TestAnyTextMixin):
     """Verify text integration."""
 
