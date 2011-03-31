c01ef076b5ae543338fe83c654963196b1a01abd
Curtis Hovey <sinzui.is@verizon.net>
2011-03-31 09:32:32 -0400

    Explicitly state where to install the javascript files.

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ from distutils.core import setup
 setup(
     name="pocketlint",
     description="Pocket-lint a composite linter and style checker.",
-    version="0.5.8",
+    version="0.5.9",
     maintainer="Curtis C. Hovey",
     maintainer_email="sinzui.is@verizon.net",
     url="https://launchpad.net/pocket-lint",
     packages=[
         'pocketlint', 'pocketlint/contrib', 'pocketlint/contrib/pyflakes'],
+    package_dir={
+        'pocketlint': 'pocketlint',
+        'pocketlint/contrib': 'pocketlint/contrib'},
+    package_data={
+        'pocketlint': ['jsreporter.js'],
+        'pocketlint/contrib': ['fulljslint.js'],
+        },
     scripts=['scripts/pocketlint'],
     )
