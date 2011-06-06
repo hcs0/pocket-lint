8c74ae32fab10955304941ae7bf20def4718cf9e
Curtis Hovey <sinzui.is@verizon.net>
2011-06-06 13:43:56 -0400

    Use seed as the js interpreter.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class SignedSDistCommand(sdist):
 setup(
     name="pocketlint",
     description="Pocket-lint a composite linter and style checker.",
-    version="0.5.18",
+    version="0.5.19",
     maintainer="Curtis C. Hovey",
     maintainer_email="sinzui.is@verizon.net",
     url="https://launchpad.net/pocket-lint",
