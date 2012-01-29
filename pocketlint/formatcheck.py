ee24d11eebcf786a946e707d6eaf64e689a5f60a
Curtis Hovey <sinzui.is@verizon.net>
2012-01-29 16:24:20 -0500

    Updated copyrights

--- pocketlint/formatcheck.py
+++ pocketlint/formatcheck.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (C) 2009-2011 - Curtis Hovey <sinzui.is at verizon.net>
+# Copyright (C) 2009-2012 - Curtis Hovey <sinzui.is at verizon.net>
 # This software is licensed under the MIT license (see the file COPYING).
 """Check for syntax and style problems."""
 
