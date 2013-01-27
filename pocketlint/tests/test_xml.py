ed79c46dbec7686c23a237a1bd9c184653855e69
Curtis Hovey <sinzui.is@verizon.net>
2013-01-27 08:31:27 -0500

    Update test to use text as is provided when the file is opened.

--- pocketlint/tests/test_xml.py
+++ pocketlint/tests/test_xml.py
@@ -39,14 +39,14 @@ ill_formed_markup = """\
 </root>
 """
 
-utf8_xml_markup = b"""\
+utf8_xml_markup = """\
 \xa0<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 <root>
   <child>hello world</child>
 </root>
 """
 
-utf8_html_markup = b"""\
+utf8_html_markup = """\
 \xa0<!DOCTYPE html PUBLIC
   "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
