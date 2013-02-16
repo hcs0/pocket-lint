9966c23b5f5f6cb7d0f8126c8ba5599f58ee0853
Curtis Hovey <sinzui.is@verizon.net>
2013-02-16 13:16:33 -0500

    Move Reporter and helper to report module.

--- pocketlint/formatcheck.py
+++ pocketlint/formatcheck.py
@@ -67,6 +67,10 @@ except ImportError:
     HAS_CSSUTILS = False
 
 from pocketlint.formatdoctest import DoctestReviewer
+from pocketlint.reporter import (
+    CSSReporterHandler,
+    Reporter,
+)
 import pocketlint.contrib.pep8 as pep8
 from pocketlint.contrib.cssccc import CSSCodingConventionChecker
 try:
@@ -93,31 +97,6 @@ JS = find_exec(['gjs', 'seed'])
 
 
 DEFAULT_MAX_LENGTH = 80
-
-
-class ConsoleHandler(logging.StreamHandler):
-    """A handler that logs to console."""
-
-    def __init__(self):
-        logging.StreamHandler.__init__(self)
-        self.stream = None  # Not used.
-
-    def emit(self, record):
-        self.__emit(record, sys.stdout)
-
-    def __emit(self, record, stream):
-        self.stream = stream
-        logging.StreamHandler.emit(self, record)
-
-    def flush(self):
-        is_flushable = self.stream and hasattr(self.stream, 'flush')
-        if is_flushable and not self.stream.closed:
-            logging.StreamHandler.flush(self)
-
-
-logger = logging.getLogger()
-logger.propagate = False
-logger.addHandler(ConsoleHandler())
 
 
 if sys.version_info >= (3,):
@@ -171,66 +150,6 @@ class PocketLintPyFlakesChecker(PyFlakesChecker):
         if node.id == 'WindowsError':
             return
         return super(PocketLintPyFlakesChecker, self).NAME(node)
-
-
-class Reporter(object):
-    """Common rules for checkers."""
-    CONSOLE = object()
-    FILE_LINES = object()
-    COLLECTOR = object()
-
-    def __init__(self, report_type, treeview=None):
-        self.report_type = report_type
-        self.file_lines_view = treeview
-        if self.file_lines_view is not None:
-            self.treestore = self.file_lines_view.get_model()
-        self.piter = None
-        self._last_file_name = None
-        self.call_count = 0
-        self.error_only = False
-        self.messages = []
-
-    def __call__(self, line_no, message, icon=None,
-                 base_dir=None, file_name=None):
-        """Report a message."""
-        if self.error_only and icon != 'error':
-            return
-        self.call_count += 1
-        args = (line_no, message, icon, base_dir, file_name)
-        if self.report_type == self.FILE_LINES:
-            self._message_file_lines(*args)
-        elif self.report_type == self.COLLECTOR:
-            self._message_collector(*args)
-        else:
-            self._message_console(*args)
-
-    def _message_console(self, line_no, message, icon=None,
-                         base_dir=None, file_name=None):
-        """Print the messages to the console."""
-        self._message_console_group(base_dir, file_name)
-        logger.error('    %4s: %s' % (line_no, message))
-
-    def _message_console_group(self, base_dir, file_name):
-        """Print the file name is it has not been seen yet."""
-        source = (base_dir, file_name)
-        if file_name is not None and source != self._last_file_name:
-            self._last_file_name = source
-            logger.error('%s' % os.path.join('./', base_dir, file_name))
-
-    def _message_file_lines(self, line_no, message, icon=None,
-                            base_dir=None, file_name=None):
-        """Display the messages in the file_lines_view."""
-        if self.piter is None:
-            mime_type = 'gnome-mime-text'
-            self.piter = self.treestore.append(
-                None, (file_name, mime_type, 0, None, base_dir))
-        self.treestore.append(
-            self.piter, (file_name, icon, line_no, message, base_dir))
-
-    def _message_collector(self, line_no, message, icon=None,
-                           base_dir=None, file_name=None):
-        self._last_file_name = (base_dir, file_name)
-        self.messages.append((line_no, message))
 
 
 class Language(object):
@@ -546,43 +465,6 @@ class XMLChecker(BaseChecker, AnyTextMixin):
             line_no += 1
             self.check_trailing_whitespace(line_no, line)
             self.check_conflicts(line_no, line)
-
-
-class CSSReporterHandler(logging.Handler):
-    """A logging handler that uses the checker to report issues."""
-
-    error_pattern = re.compile(
-        r'(?P<issue>[^(]+): \((?P<text>[^,]+, [^,]+), (?P<lineno>\d+).*')
-    message_pattern = re.compile(
-        r'(?P<issue>[^:]+:[^:]+): (?P<text>.*) (?P<lineno>0)$')
-
-    def __init__(self, checker):
-        logging.Handler.__init__(self, logging.INFO)
-        self.checker = checker
-
-    def handleError(self, record):
-        pass
-
-    def emit(self, record):
-        if record.levelname == 'ERROR':
-            icon = 'error'
-        else:
-            icon = 'info'
-        matches = self.checker.message_pattern.search(record.getMessage())
-        if matches is None:
-            matches = self.error_pattern.search(record.getMessage())
-        try:
-            issue = matches.group('issue')
-            text = matches.group('text')
-            if 'Level 2.1' in issue and issue.endswith('rem'):
-                # Do not suggest that using CSS3 is bad.
-                return
-            line_no = matches.group('lineno')
-            message = "%s: %s" % (issue, text)
-        except AttributeError:
-            line_no = 0
-            message = record.getMessage()
-        self.checker.message(int(line_no), message, icon=icon)
 
 
 class CSSChecker(BaseChecker, AnyTextMixin):
