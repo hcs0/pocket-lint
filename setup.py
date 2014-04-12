63cdae495d22c5e1c8748cbaeca84868d60ddc4b
Adi Roiban <adi.roiban@chevah.com>
2014-04-12 22:38:14 +0300

    Update version.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ class Check(Command):
 setup(
     name="pocketlint",
     description="Pocket-lint a composite linter and style checker.",
-    version="1.4.4.c5",
+    version="1.4.4.c6",
     maintainer="Curtis C. Hovey",
     maintainer_email="sinzui.is@gmail.com",
     url="https://launchpad.net/pocket-lint",
